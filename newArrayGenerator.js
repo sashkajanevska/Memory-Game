243473ec168d8b4c8ca14ea045b8f4b02e02097c
sashkajanevska <janevska_sashka@yahoo.com>
2025-04-11 18:43:03 +0200

    images updated

--- newArrayGenerator.js
+++ newArrayGenerator.js
@@ -1,28 +1,28 @@
 const albumCovers = [
-    "images/_Aerosmith.png",
-    "images/_DireStraits.png",
-    "images/_FleetwoodMac.png",
-    "images/_GunsNRoses.png",
-    "images/_JeffersonAirplane.png",
-    "images/_JimiHendrixExperience.png",
-    "images/_Metallica.png",
-    "images/_Oasis.png",
-    "images/_PinkFloyd.png",
-    "images/_TheBeatles.png",
-    "images/_TheDoors.png",
-    "images/_TheRollingStones.png"
+  "images/_Aerosmith.png",
+  "images/_DireStraits.png",
+  "images/_FleetwoodMac.png",
+  "images/_GreenDay.png",
+  "images/_GunsNRoses.png",
+  "images/_JeffersonAirplane.png",
+  "images/_JimiHendrixExperience.png",
+  "images/_LedZeppelin.png",
+  "images/_Oasis.png",
+  "images/_PinkFloyd.png",
+  "images/_TheBeatles.png",
+  "images/_TheDoors.png",
 ];
 
 export function newCoversArray() {
-    const coversFullList = [...albumCovers, ...albumCovers];
-    const listLength = coversFullList.length;
-    let newArray = [];
+  const coversFullList = [...albumCovers, ...albumCovers];
+  const listLength = coversFullList.length;
+  let newArray = [];
 
-    for (let i = 0; i < listLength; i++) {
-        let randomIndex = Math.floor(Math.random() * coversFullList.length);
-        let splicedItem = coversFullList.splice(randomIndex, 1);
-        newArray.push(splicedItem);
-    }
-    let result = newArray.flat();
-    return result;
-};
+  for (let i = 0; i < listLength; i++) {
+    let randomIndex = Math.floor(Math.random() * coversFullList.length);
+    let splicedItem = coversFullList.splice(randomIndex, 1);
+    newArray.push(splicedItem);
+  }
+  let result = newArray.flat();
+  return result;
+}
